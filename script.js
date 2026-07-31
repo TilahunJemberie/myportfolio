376440dd2ee7812558ba878800dc97c440d59922
TilahunJemberie <tilahun3274@gmail.com>
2026-07-31 08:52:15 +0300

    Update Portfolio

--- script.js
+++ script.js
@@ -104,3 +104,26 @@ setTimeout(typeText,80);
 
 
 typeText();
+// Scroll To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 ||
+        document.documentElement.scrollTop > 200) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
